78f15d22962c09053e4088fd65fb0a3141213617
ksirf <vipul153@gmail.com>
2023-10-16 10:30:45 -0500

    Update tool_download.sh

--- tool_download.sh
+++ tool_download.sh
@@ -12,11 +12,11 @@ git clone https://github.com/knavesec/CredMaster.git tools/CredMaster
 
 # Download Nuclei
 echo "Downloading Nuclei..."
-wget https://github.com/projectdiscovery/nuclei/releases/download/v2.9.2/nuclei_2.9.2_linux_amd64.zip && unzip nuclei_2.9.2_linux_amd64.zip && rm nuclei_2.9.2_linux_amd64.zip
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 
 # Download httpx
 echo "Downloading httpx..."
-wget https://github.com/projectdiscovery/httpx/releases/download/v1.2.9/httpx_1.2.9_linux_amd64.zip && unzip httpx_1.2.9_linux_amd64.zip && rm httpx_1.2.9_linux_amd64.zip
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 
 # Download Cloud_enum
 echo "Downloading Cloud_enum..."
